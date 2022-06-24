b1e3a4104f062b605aba5a1e368044506fbc18af
LamLee <admin@Mac-cua-Lam.local>
2022-06-24 15:05:06 +0700

    xây dựng header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,4 +1,8 @@
 import className from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+import images from '~/assets/images';
 import styles from './Header.module.scss';
 
 const cx = className.bind(styles);
@@ -6,7 +10,20 @@ const cx = className.bind(styles);
 function Sidebar() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="Tiktok"></img>
+                <div className={cx('search')}>
+                    <input placeholder="Search account and videos" spellCheck={false}></input>
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
         </header>
     );
 }
